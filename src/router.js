62e72a29450e747af3a9560305f8dfa04136cb3d
jzen <jzenzhang@foxmail.com>
2020-02-09 00:36:32 +0800

    :update: 优化路由配置

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
 import HomeLayout from './layout/Home/Home'
-import Aboutme from './layout/Aboutme/Aboutme'
+import AboutmeLayout from './layout/Aboutme/Aboutme'
 import ChatRoom from './components/ChatRoom/ChatRoom'
 import { drag } from './utils/index'
 var ChatModule = drag(ChatRoom, { bottom: '50px', right: '10px' })
@@ -11,11 +11,10 @@ function RouterPage() {
     <BrowserRouter>
       <Switch>
         <Route path="/home" exact component={HomeLayout} />
-        <Route path="/about_me" component={Aboutme} />
+        <Route path="/about_me" component={AboutmeLayout} />
         <Redirect from="/*" to="/home" />
       </Switch>
       <Route path="/" component={ChatModule}></Route>
-      {/* <Route exact path="/about_me" component={Aboutme}></Route> */}
     </BrowserRouter>
   )
 }
